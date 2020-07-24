793d399a25cf82a2f80deedad41ba2fcf2478ce5
Clevergua <CleverGua@outlook.com>
2020-07-24 17:11:26 +0800

    .vox reader

---
+++ VoxTranslator/Program.cs
@@ -0,0 +1,28 @@
+﻿using CsharpVoxReader;
+using System;
+using System.IO;
+
+namespace VoxTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = Directory.GetCurrentDirectory();
+            Console.WriteLine(rootPath);
+
+            DirectoryInfo dirInfo = new DirectoryInfo(rootPath);
+            var loader = new VoxelLoader();
+
+            foreach (var file in dirInfo.GetFiles("*.vox"))
+            {
+                Console.WriteLine(file.FullName);
+                VoxReader reader = new VoxReader(file.FullName, loader);
+                reader.Read();
+            }
+
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ VoxTranslator/VoxelLoader.cs
@@ -0,0 +1,57 @@
+﻿using CsharpVoxReader;
+using CsharpVoxReader.Chunks;
+using System;
+using System.Collections.Generic;
+
+namespace VoxTranslator
+{
+    class VoxelLoader : IVoxLoader
+    {
+        public void LoadModel(int sizeX, int sizeY, int sizeZ, byte[,,] data)
+        {
+            Console.WriteLine($"({sizeX},{sizeY},{sizeZ})");
+            for (int x = 0; x < data.GetLength(0); x++)
+            {
+                for (int y = 0; y < data.GetLength(1); y++)
+                {
+                    for (int z = 0; z < data.GetLength(2); z++)
+                    {
+                        Console.Write($"{data[x, y, z]},");
+                    }
+                }
+            }
+        }
+
+        public void LoadPalette(uint[] palette)
+        {
+        }
+
+        public void NewGroupNode(int id, Dictionary<string, byte[]> attributes, int[] childrenIds)
+        {
+        }
+
+        public void NewLayer(int id, Dictionary<string, byte[]> attributes)
+        {
+        }
+
+        public void NewMaterial(int id, Dictionary<string, byte[]> attributes)
+        {
+        }
+
+        public void NewShapeNode(int id, Dictionary<string, byte[]> attributes, int[] modelIds, Dictionary<string, byte[]>[] modelsAttributes)
+        {
+        }
+
+        public void NewTransformNode(int id, int childNodeId, int layerId, Dictionary<string, byte[]>[] framesAttributes)
+        {
+        }
+
+        public void SetMaterialOld(int paletteId, MaterialOld.MaterialTypes type, float weight, MaterialOld.PropertyBits property, float normalized)
+        {
+        }
+
+        public void SetModelCount(int count)
+        {
+        }
+    }
+}
